810da2c6b9b2e21e7a5d217292d1f11e3537750e
Nihar Mondal <developernihar@gmail.com>
2025-07-18 18:38:58 +0600

    update: config/index.ts file

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,6 @@ export const config = {
 	// cloud config
 	cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
 	cloud_upload_preset: process.env.NEXT_PUBLIC_UPLOAD_PRESET,
-	cloud_api_key: process.env.CLOUDINARY_API_KEY,
-	cloud_api_secret: process.env.CLOUDINARY_API_SECRET,
+	cloud_api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+	cloud_api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET,
 };
