f2b413825b0fd74baf7ed09aad12f43805b2f089
Nihar Mondal <developernihar@gmail.com>
2025-08-03 14:56:59 +0600

    style: added free tag on course card and hover effect to course photo

--- src/app/(root)/courses/course-list.tsx
+++ src/app/(root)/courses/course-list.tsx
@@ -15,7 +15,7 @@ export default function CourseList({ searchValue }: CourseListProps) {
 
 	if (isLoading) return <AppLoading />;
 	return (
-		<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 ">
+		<div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 ">
 			{!courses?.result.length && <p>No course found!</p>}
 			{courses?.result?.map((course) => (
 				<CourseCard course={course} key={course._id} />
